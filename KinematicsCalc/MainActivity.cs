8f4d3a230801ce4097e7ba0dcddcc7c7b808eb0a
Dennis Aziaya <aziayadennis@gmail.com>
2020-01-22 23:17:43 +0300

    spiiner functionality added

--- KinematicsCalc/MainActivity.cs
+++ KinematicsCalc/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
 
 namespace KinematicsCalc
 {
@@ -21,6 +22,15 @@ namespace KinematicsCalc
 
         private string[] listOfMissingVariable;
         private string[] listOfCalculations;
+
+        private double finalAnswer;
+        private double variable1;
+        private double variable2;
+        private double variable3;
+
+        private string variableToCalculate;
+        private string calculationToUse;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -32,9 +42,9 @@ namespace KinematicsCalc
 
             _textViewchooseVariable = FindViewById<TextView>(Resource.Id.txtChooseVariable);
             _textViewChooseCalculation = FindViewById<TextView>(Resource.Id.txtChooseCalculation);
-            _editTextField1 = FindViewById<EditText>(Resource.Id.txtField1);
-            _editTextField2 = FindViewById<EditText>(Resource.Id.txtField2);
-            _editTextField3 = FindViewById<EditText>(Resource.Id.txtField3);
+            _editTextField1 = FindViewById<EditText>(Resource.Id.editTxtField1);
+            _editTextField2 = FindViewById<EditText>(Resource.Id.editTxtField2);
+            _editTextField3 = FindViewById<EditText>(Resource.Id.editTxtField3);
             _textViewResult = FindViewById<TextView>(Resource.Id.txtResult);
             _buttonCalculate = FindViewById<Button>(Resource.Id.btnCalculate);
             _spinnerChooseCalculation = FindViewById<Spinner>(Resource.Id.spnChooseCalculation);
@@ -53,11 +63,10 @@ namespace KinematicsCalc
             listOfCalculations = new string[]
             {
                 "choose missing variable",
-                "d = vi*t",
-                "Initial velocity",
-                "Acceleration",
-                "Displacement",
-                "Time"
+                "d = vi*t+0.5*a*t^2",
+                "vf^2 = vi^2 + 2*a*d",
+                "vf = vi + a*t",
+                "d = 0.5*(vi=vf)*t"
             };
 
             _buttonCalculate.Click += _buttonCalculate_Click;
@@ -66,6 +75,43 @@ namespace KinematicsCalc
         private void _buttonCalculate_Click(object sender, System.EventArgs e)
         {
             throw new System.NotImplementedException();
+        }
+
+        void setMissingVariableSpinner()
+        {
+            var adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, listOfMissingVariable);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            _spinnerChooseVariable.Adapter = adapter;
+        }
+
+        void chooseFromMissingVariableSpinner()
+        {
+            _spinnerChooseVariable.ItemSelected += _spinnerChooseVariable_ItemSelected;
+        }
+
+        private void _spinnerChooseVariable_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            variableToCalculate = _spinnerChooseVariable.SelectedItem.ToString();
+            _textViewchooseVariable.Text = variableToCalculate;
+            chooseFromCalculationSpinner();
+        }
+
+        void setCalculationVariableSpinner()
+        {
+            var adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, listOfCalculations);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            _spinnerChooseCalculation.Adapter = adapter;
+        }
+
+        void chooseFromCalculationSpinner()
+        {
+            _spinnerChooseCalculation.ItemSelected += _spinnerChooseCalculation_ItemSelected;
+        }
+
+        private void _spinnerChooseCalculation_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            calculationToUse = _spinnerChooseCalculation.SelectedItem.ToString();
+            _textViewChooseCalculation.Text = calculationToUse;
         }
     }
 }
